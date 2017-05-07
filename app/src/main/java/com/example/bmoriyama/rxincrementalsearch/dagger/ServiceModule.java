c9fd43656708b735a3b9237dbda83117b6f454c2
bmoriyama <brianmoriyama@gmail.com>
2017-05-07 01:53:16 -0700

    Rewire StackOverflowService for testing with DI

--- app/src/main/java/com/example/bmoriyama/rxincrementalsearch/dagger/ServiceModule.java
+++ app/src/main/java/com/example/bmoriyama/rxincrementalsearch/dagger/ServiceModule.java
@@ -1,14 +1,32 @@
 package com.example.bmoriyama.rxincrementalsearch.dagger;
 
+import com.example.bmoriyama.rxincrementalsearch.service.StackOverflowAPI;
 import com.example.bmoriyama.rxincrementalsearch.service.StackOverflowService;
 
 import dagger.Module;
 import dagger.Provides;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class ServiceModule {
+
+    private String BASE_URL = "https://api.stackexchange.com/";
+
     @Provides
-    StackOverflowService provideStackOverflowService() {
-        return new StackOverflowService();
+    StackOverflowService provideStackOverflowService(StackOverflowAPI stackOverflowAPI) {
+        return new StackOverflowService(stackOverflowAPI);
+    }
+
+    @Provides
+    StackOverflowAPI provideStackOverflowAPI() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit.create(StackOverflowAPI.class);
     }
 }
--- app/src/main/java/com/example/bmoriyama/rxincrementalsearch/service/StackOverflowService.java
+++ app/src/main/java/com/example/bmoriyama/rxincrementalsearch/service/StackOverflowService.java
@@ -1,35 +1,21 @@
 package com.example.bmoriyama.rxincrementalsearch.service;
 
-import android.net.Uri;
-import android.util.Log;
-
 import com.example.bmoriyama.rxincrementalsearch.model.SearchResponse;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class StackOverflowService {
-    private String BASE_URL = "https://api.stackexchange.com/";
 
     private StackOverflowAPI stackOverflowAPI;
 
-    public StackOverflowService() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        stackOverflowAPI = retrofit.create(StackOverflowAPI.class);
+    public StackOverflowService(StackOverflowAPI stackOverflowAPI) {
+        this.stackOverflowAPI = stackOverflowAPI;
     }
 
     public Observable<SearchResponse> searchQuestions(String inTitle) throws UnsupportedEncodingException {
-        Log.i("query", Uri.encode(inTitle));
         return stackOverflowAPI.searchQuestions(URLEncoder.encode(inTitle, "UTF-8"));
     }
 }
